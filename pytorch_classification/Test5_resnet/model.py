d1734bf2bdf7e0dfb5f35cc83352d6396b20a5fb
Hai-rf <1224483724@qq.com>
2023-07-26 23:10:39 +0800

    add annotation

--- pytorch_classification/Test5_resnet/model.py
+++ pytorch_classification/Test5_resnet/model.py
@@ -1,24 +1,31 @@
 import torch.nn as nn
 import torch
+import torchvision.models.resnet
 
 
 class BasicBlock(nn.Module):
-    expansion = 1
+    '''
+    ResNet-18,34所对应的残差结构
+    '''
+    expansion = 1 #主分支卷积核的个数的变化，18，32对应的两层卷积核个数无变化，为1
 
     def __init__(self, in_channel, out_channel, stride=1, downsample=None, **kwargs):
+        '''
+        downsample:用于shortcut虚线, 需要在不同的卷积组之间将特征矩阵形状变形
+        '''
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel,
-                               kernel_size=3, stride=stride, padding=1, bias=False)
+                               kernel_size=3, stride=stride, padding=1, bias=False) #使用BN，不需要bias
         self.bn1 = nn.BatchNorm2d(out_channel)
         self.relu = nn.ReLU()
         self.conv2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel,
                                kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channel)
-        self.downsample = downsample
+        self.downsample = downsample #指定下采样方法
 
     def forward(self, x):
-        identity = x
-        if self.downsample is not None:
+        identity = x # identity作为旁路的输出
+        if self.downsample is not None: # None表示当前旁路无操作， not None 表示当前旁路需要变形输入特征
             identity = self.downsample(x)
 
         out = self.conv1(x)
@@ -41,18 +48,21 @@ class Bottleneck(nn.Module):
     这么做的好处是能够在top1上提升大概0.5%的准确率。
     可参考Resnet v1.5 https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch
     """
-    expansion = 4
+    expansion = 4 #主分支卷积核的个数的变化，50，101，152对应的第三层卷积核是前两层卷积核个数的4倍，即特征通道增加为4倍
 
     def __init__(self, in_channel, out_channel, stride=1, downsample=None,
                  groups=1, width_per_group=64):
+        '''
+        out_channel是模块中第一与第二层卷积的输出通道，最后一层的输出通道在此基础上根据expansion扩展
+        '''
         super(Bottleneck, self).__init__()
-
         width = int(out_channel * (width_per_group / 64.)) * groups
 
         self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=width,
                                kernel_size=1, stride=1, bias=False)  # squeeze channels
         self.bn1 = nn.BatchNorm2d(width)
         # -----------------------------------------
+        # 不同残差组连接处的模块第二层卷积的步距变为2，此处的步距需要作为变量根据调用位置调整
         self.conv2 = nn.Conv2d(in_channels=width, out_channels=width, groups=groups,
                                kernel_size=3, stride=stride, bias=False, padding=1)
         self.bn2 = nn.BatchNorm2d(width)
@@ -65,7 +75,7 @@ class Bottleneck(nn.Module):
 
     def forward(self, x):
         identity = x
-        if self.downsample is not None:
+        if self.downsample is not None: # downsample即旁路的操作
             identity = self.downsample(x)
 
         out = self.conv1(x)
@@ -88,25 +98,26 @@ class Bottleneck(nn.Module):
 class ResNet(nn.Module):
 
     def __init__(self,
-                 block,
-                 blocks_num,
+                 block, # 根据网络定义的不同层数传入不同的block(basic/bottleneck)
+                 blocks_num, # list各个残差组中模块个数
                  num_classes=1000,
-                 include_top=True,
+                 include_top=True,# 作为与其他网络结构结合的接口
                  groups=1,
                  width_per_group=64):
         super(ResNet, self).__init__()
         self.include_top = include_top
-        self.in_channel = 64
+        self.in_channel = 64 # 初始的卷积和pooling后的通道数，所有层数的网络设置都一样
 
         self.groups = groups
         self.width_per_group = width_per_group
 
         self.conv1 = nn.Conv2d(3, self.in_channel, kernel_size=7, stride=2,
-                               padding=3, bias=False)
+                               padding=3, bias=False)# padding是根据输出feature高，宽为原来一半进行计算得到的，conv1高，宽减半
         self.bn1 = nn.BatchNorm2d(self.in_channel)
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, blocks_num[0])
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1) # max pool高，宽减半
+         
+        self.layer1 = self._make_layer(block, 64, blocks_num[0]) # ResNet的一个卷积组即一个残差模块
         self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)
         self.layer3 = self._make_layer(block, 256, blocks_num[2], stride=2)
         self.layer4 = self._make_layer(block, 512, blocks_num[3], stride=2)
@@ -119,6 +130,10 @@ class ResNet(nn.Module):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
 
     def _make_layer(self, block, channel, block_num, stride=1):
+        '''
+        block: 残差模块实例对象
+        channel:残差模块第一层通道数（深度）
+        '''
         downsample = None
         if stride != 1 or self.in_channel != channel * block.expansion:
             downsample = nn.Sequential(
@@ -140,7 +155,10 @@ class ResNet(nn.Module):
                                 groups=self.groups,
                                 width_per_group=self.width_per_group))
 
-        return nn.Sequential(*layers)
+        return nn.Sequential(*layers) 
+        # *非关键字参数的形式传入，在nn.Sequential()中,接收的参数是一个层的列表。如果不使用*号,需要将层列表封装成另一个列表再传递，
+        # 使用*号可以直接将层列表中的元素作为位置参数传递给nn.Sequential(),避免创建多余的列表
+        # *号的作用是展开列表/元组,将其元素作为位置参数传递,使代码更简洁。
 
     def forward(self, x):
         x = self.conv1(x)
@@ -155,7 +173,7 @@ class ResNet(nn.Module):
 
         if self.include_top:
             x = self.avgpool(x)
-            x = torch.flatten(x, 1)
+            x = torch.flatten(x, 1) # x:（depth,1,1)->(depth,) 全连接之前将特征展平为一维
             x = self.fc(x)
 
         return x
